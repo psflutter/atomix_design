129b44d233911122ec33495064e62ec57e5714a2
Pradeep Sharma <mrksphp@gmail.com>
2024-12-28 16:18:07 +0530

    add: add color file

---
+++ lib/theme/color/atomix_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract final class AtomixColors {
+  static const Color transparent = Color(0x00000000);
+  static const Color color000000 = Color(0xFF000000);
+  static const Color colorFFFFFF = Color(0xFFFFFFFF);
+}
